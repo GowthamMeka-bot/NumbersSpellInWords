4bdbcb1c831c46fe835e75fa97aaac72adff4cb2
Gowtham Meka <P10458945@capita.co.uk>
2020-05-09 21:14:18 +0530

    Committing test library to master

---
+++ NumberToWord.Library.Tests/NumberToWordTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberToWord.Library.Tests
+{
+    [TestFixture]
+    public class NumberToWordTests
+    {
+        [Test]
+        public void Should_Return_FiftyFive_When_55()
+        {
+            // Arrange
+            int input = 55;
+            // Act
+            string output = SpellNumberToWord.SpellTheNumber(input);
+            // Assert
+            Assert.AreEqual("fifty five",output);
+        }
+    }
+}
